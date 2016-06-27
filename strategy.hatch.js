131186d2d64b4472e3d54e5782a28bf0e5a94a73
Andrey Oleynikov <aoleynikov@weezlabs.com>
2016-06-27 19:54:35 +0300

    made hatch level independent

--- strategy.hatch.js
+++ strategy.hatch.js
@@ -8,12 +8,12 @@ var desired_population =
 	'repairer': {'count': 1, type: 'worker'}
 };
 
-var maxEnergy = function(spawn){
-		var fromSpawn = 300; // spawn
-		var level = spawn.room.controller.level;
-		var fromExts = spawn.room.find(STRUCTURE_EXTENSION).length * EXTENSION_ENERGY_CAPACITY[level];
-		return fromSpawn + fromExts;
-	};
+var maxEnergy = function(spawn) {
+	var fromSpawn = 300; // spawn
+	var level = spawn.room.controller.level;
+	var fromExts = spawn.room.find(STRUCTURE_EXTENSION).length * EXTENSION_ENERGY_CAPACITY[level];
+	return fromSpawn + fromExts;
+};
 
 var composeWorker = function(spawn) {
 	var total = maxEnergy(spawn) - BODYPART_COST['move'];
