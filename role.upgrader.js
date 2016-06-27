971c23269790a159f7552f20aa06e738ffabdea6
Andrey Oleynikov <aoleynikov@weezlabs.com>
2016-06-27 16:51:33 +0300

    fixed upgrader + populations

--- role.upgrader.js
+++ role.upgrader.js
@@ -4,13 +4,19 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.carry.energy < creep.carryCapacity) {
-            src_find.run(creep);
+
+        if(creep.memory.working && creep.carry.energy == 0) {
+            creep.memory.working = false;
         }
-        else {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
-            }
+        if(!creep.memory.working && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.working = true;
+        }
+
+        if (creep.memory.working && creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(creep.room.controller);
+        }
+        else (creep.carry.energy > 0){
+            src_find.run(creep);
         }
 	}
 };
