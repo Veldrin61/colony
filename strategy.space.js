42e06f02123d28b1b564238f155c8b7cc127660b
Andrey Oleynikov <aoleynikov@weezlabs.com>
2016-06-29 10:54:04 +0300

    console.log

--- strategy.space.js
+++ strategy.space.js
@@ -4,11 +4,13 @@ var space = {
 
 		var leftRoomKey = Game.map.describeExits('E36S7')['7'];
 		rooms.push(Game.rooms[leftRoomKey]);
+		console.log(rooms);
 
 		return {
 			find: function(query){
 				var result = [];
 				for(var i=0; i<rooms.length; ++i){
+
 					result.concat(rooms[i].find(query));
 				}
 				return result;
